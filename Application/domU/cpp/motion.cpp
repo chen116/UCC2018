a4c77ce631af98dcd23aee6ce0feb126fea02a6b
Victor Chen <yvictorck@gmail.com>
2018-08-08 17:39:49 -0400

    a

--- Application/domU/cpp/motion.cpp
+++ Application/domU/cpp/motion.cpp
@@ -5,28 +5,117 @@
 #include <opencv2/tracking.hpp>
 #include <opencv2/core/ocl.hpp>
 #include <unistd.h>
+#include "pacer.h"
 
 using namespace cv;
 using namespace std;
  
 int main(int argc, char **argv) {
+Pacer mypacer;
+mypacer.setWindowSize(10);
+mypacer.setItem("box_entry");
+mypacer.setItem("frame_number_entry");
+mypacer.setItem("frame_size");
 
-    Mat frame, gray, frameDelta, thresh, firstFrame;
-    vector<vector<Point> > cnts;
-    VideoCapture camera("rollcar.3gp"); //open camera
-    //set the video size to 512x288 to process faster
-    // camera.set(3, 512);
-    // camera.set(4, 288);
+char * item;
+printf("My Dom ID: %d\n",mypacer.getDomid() );
+printf("waiting for dom0...\n");
+while (strcmp("init",item)!=0)
+{
+    item = mypacer.readItem("frame_number_entry");
+} 
 
-    sleep(3);
-    camera.read(frame);
+item = mypacer.readItem("box_entry");
+string init_video_data = string(item);
+istringstream iss(item);
+vector<string> init_video_data_vec(std::istream_iterator<std::string>{iss}, std::istream_iterator<std::string>());
 
-    //convert to grayscale and set the first frame
-    cvtColor(frame, firstFrame, COLOR_BGR2GRAY);
-    GaussianBlur(firstFrame, firstFrame, Size(21, 21), 0);
+int hw_size,lw_size;
+if (init_video_data_vec[0]=="init")
+{
+    hw_size=stoi(init_video_data_vec[1]);
+    lw_size=stoi(init_video_data_vec[2]);
+}
+vector<int> vidarray_binary;
+std::stringstream ss(init_video_data_vec[3]);
+int i;
+while (ss >> i)
+{
+    vidarray_binary.push_back(i);
+    if (ss.peek() == ',') ss.ignore();
+}
 
-    while(camera.read(frame)) {
 
+
+vector<Mat> car;
+vector<Mat> no_car;
+for (int k = 0; k < 200; ++k)
+{
+    Mat frame;
+    cap >> frame;
+    if (k>=20 and k <50)
+    {
+        car.push_back(frame);
+    }
+    if (k >= 140)
+    {
+        no_car.push_back(frame);
+    }
+}
+vector<Mat> flipcar = car; 
+reverse(flipcar.begin(),flipcar.end());
+for (int k=0;k<flipcar.size();k++)
+{
+    car.push_back(flipcar[k]);
+}
+
+std::vector<Mat> vidarray;
+for (i=0; i< vidarray_binary.size(); i++)
+{
+    if (vidarray_binary.at(i))
+    {
+        for (int k=0; k< car.size(); k++) vidarray.push_back(car.at(k));
+    }
+    else
+    {
+        for (int k=0; k< no_car.size(); k++) vidarray.push_back(no_car.at(k));
+    }
+}
+
+string box_coords = "0 0 0 0";
+mypacer.writeItem("box_entry",box_coords.c_str());
+mypacer.writeItem("frame_number_entry","ready");
+printf("ready...\n");
+
+
+int frame_num;
+int prev_frame_num = -1;
+int frame_size = vidarray_binary[0];
+int detect_car = vidarray_binary[0];
+int prev_frame_size = 0;
+
+Mat frame, gray, frameDelta, thresh, firstFrame;
+vector<vector<Point> > cnts;
+while (strcmp("done",item)!=0)
+{
+
+item=mypacer.readItem("frame_number_entry");
+frame_num = atoi(item);
+if (frame_num > prev_frame_num) 
+{
+
+    prev_frame_num = frame_num;
+    frame = vidarray[frame_num];
+    if (detect_car) frame_size = hw_size;
+    else frame_size = lw_size;
+    if(frame_num == 0)
+    {
+        //convert to grayscale and set the first frame
+        cvtColor(frame, firstFrame, COLOR_BGR2GRAY);
+        GaussianBlur(firstFrame, firstFrame, Size(21, 21), 0);
+    }
+    else
+    {
         //convert to grayscale
         cvtColor(frame, gray, COLOR_BGR2GRAY);
         GaussianBlur(gray, gray, Size(21, 21), 0);
@@ -42,127 +131,80 @@ int main(int argc, char **argv) {
             if(contourArea(cnts[i]) < 500) {
                 continue;
             }
-            putText(frame, "Motion Detected", Point(10, 20), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0,0,255),2);
+            // putText(frame, "Motion Detected", Point(10, 20), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0,0,255),2);
+            printf("Motion Detected at frame%d\n",frame_num );
         }
-    
-        imshow("Camera", frame);
+        mypacer.beat();
+        mypacer.writeInstantHeartRate();
+
+        // imshow("Camera", frame);
         
-        if(waitKey(1) == 27){
-            //exit if ESC is pressed
-            break;
-        }
+        // if(waitKey(1) == 27){
+        //     //exit if ESC is pressed
+        //     break;
+        // }
     
     }
 
     return 0;
 }
-
+// /*
+// * To compile: g++ opencv_test2.cpp -o opencv_test2 $(pkg-config --cflags --libs opencv)
+// */
 // #include <opencv2/opencv.hpp>
 // #include <opencv2/tracking.hpp>
 // #include <opencv2/core/ocl.hpp>
- 
+// #include <unistd.h>
+
 // using namespace cv;
 // using namespace std;
  
-// // Convert to string
-// #define SSTR( x ) static_cast< std::ostringstream & >( \
-// ( std::ostringstream() << std::dec << x ) ).str()
- 
-// int main(int argc, char **argv)
-// {
-//     // List of tracker types in OpenCV 3.4.1
-//     string trackerTypes[6] = {"BOOSTING", "MIL", "KCF", "TLD","MEDIANFLOW", "GOTURN"};
-//     // vector <string> trackerTypes(types, std::end(types));
- 
-//     // Create a tracker
-//     string trackerType = trackerTypes[2];
- 
-//     Ptr<Tracker> tracker;
- 
-//     #if (CV_MINOR_VERSION < 3)
-//     {
-//         tracker = Tracker::create(trackerType);
-//     }
-//     #else
-//     {
-//         if (trackerType == "BOOSTING")
-//             tracker = TrackerBoosting::create();
-//         if (trackerType == "MIL")
-//             tracker = TrackerMIL::create();
-//         if (trackerType == "KCF")
-//             tracker = TrackerKCF::create();
-//         if (trackerType == "TLD")
-//             tracker = TrackerTLD::create();
-//         if (trackerType == "MEDIANFLOW")
-//             tracker = TrackerMedianFlow::create();
-//         if (trackerType == "GOTURN")
-//             tracker = TrackerGOTURN::create();
+// int main(int argc, char **argv) {
 
-//     }
-//     #endif
-//     // Read video
-//     VideoCapture video("rollcar.3gp");
-     
-//     // Exit if video is not opened
-//     if(!video.isOpened())
-//     {
-//         cout << "Could not read video file" << endl; 
-//         return 1; 
-//     } 
- 
-//     // Read first frame 
-//     Mat frame; 
-//     bool ok = video.read(frame); 
- 
-//     // Define initial bounding box 
-//     Rect2d bbox(287, 23, 86, 320); 
- 
-//     // Uncomment the line below to select a different bounding box 
-//     // bbox = selectROI(frame, false); 
-//     // Display bounding box. 
-//     rectangle(frame, bbox, Scalar( 255, 0, 0 ), 2, 1 ); 
- 
-//     imshow("Tracking", frame); 
-//     tracker->init(frame, bbox);
-     
-//     while(video.read(frame))
-//     {     
-//         // Start timer
-//         double timer = (double)getTickCount();
-         
-//         // Update the tracking result
-//         bool ok = tracker->update(frame, bbox);
-         
-//         // Calculate Frames per second (FPS)
-//         float fps = getTickFrequency() / ((double)getTickCount() - timer);
-         
-//         if (ok)
-//         {
-//             // Tracking success : Draw the tracked object
-//             rectangle(frame, bbox, Scalar( 255, 0, 0 ), 2, 1 );
-//               printf("motion detected\n");
+//     Mat frame, gray, frameDelta, thresh, firstFrame;
+//     vector<vector<Point> > cnts;
+//     VideoCapture camera(0); //open camera
+    
+//     //set the video size to 512x288 to process faster
+//     camera.set(3, 512);
+//     camera.set(4, 288);
+
+//     sleep(3);
+//     camera.read(frame);
+
+//     //convert to grayscale and set the first frame
+//     cvtColor(frame, firstFrame, COLOR_BGR2GRAY);
+//     GaussianBlur(firstFrame, firstFrame, Size(21, 21), 0);
+
+//     while(camera.read(frame)) {
+
+//         //convert to grayscale
+//         cvtColor(frame, gray, COLOR_BGR2GRAY);
+//         GaussianBlur(gray, gray, Size(21, 21), 0);
+
+//         //compute difference between first frame and current frame
+//         absdiff(firstFrame, gray, frameDelta);
+//         threshold(frameDelta, thresh, 25, 255, THRESH_BINARY);
+        
+//         dilate(thresh, thresh, Mat(), Point(-1,-1), 2);
+//         findContours(thresh, cnts, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+//         for(int i = 0; i< cnts.size(); i++) {
+//             if(contourArea(cnts[i]) < 500) {
+//                 continue;
+//             }
+
+//             putText(frame, "Motion Detected", Point(10, 20), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0,0,255),2);
 //         }
-//         else
-//         {
-//             // Tracking failure detected.
-//             putText(frame, "Tracking failure detected", Point(100,80), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0,0,255),2);
-//         }
-         
-//         // Display tracker type on frame
-//         putText(frame, trackerType + " Tracker", Point(100,20), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50,170,50),2);
-         
-//         // Display FPS on frame
-//         putText(frame, "FPS : " + SSTR(int(fps)), Point(100,50), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50,170,50), 2);
- 
-//         // Display frame.
-//         imshow("Tracking", frame);
-         
-//         // Exit if ESC pressed.
-//         int k = waitKey(1);
-//         if(k == 27)
-//         {
+    
+//         imshow("Camera", frame);
+        
+//         if(waitKey(1) == 27){
+//             //exit if ESC is pressed
 //             break;
 //         }
- 
+    
 //     }
+
+//     return 0;
 // }
