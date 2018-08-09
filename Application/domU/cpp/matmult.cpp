8cf141edcf31e97f87657d708b2706169a18a355
Victor Chen <yvictorck@gmail.com>
2018-08-09 15:56:05 -0400

    a

--- Application/domU/cpp/matmult.cpp
+++ Application/domU/cpp/matmult.cpp
@@ -31,27 +31,6 @@ void matmult(int ** ptr1,int ** ptr2, int ** ptr3,int N){
 
 int main(int argc, char** argv)
 {
-    // create a pacer instant
-    Pacer mypacer;
-    // set window size for heartbeat and create heartbeat instance
-    mypacer.setWindowSize(5);
-    mypacer.setItem("box_entry");
-    mypacer.setItem("frame_number_entry");
-    mypacer.setItem("frame_size");
-    char * item;
-    printf("My Dom ID: %d\n",mypacer.getDomid() );
-    printf("waiting for dom0...\n");
-    while (strcmp("init",item)!=0)
-    {
-        item = mypacer.readItem("frame_number_entry");
-    } 
-
-    mypacer.writeItem("frame_number_entry","ready");
-    printf("ready...\n");
-
-    while (strcmp("done",item)!=0 && strcmp("ready",item)!=0)
-    {
-        item=mypacer.readItem("frame_number_entry");
         int **ptr1, **ptr2, **ptr3;
         int  col1, row2, col2;
         int N=300;
@@ -78,10 +57,32 @@ int main(int argc, char** argv)
             }
         }
 
+    // create a pacer instant
+    Pacer mypacer;
+    // set window size for heartbeat and create heartbeat instance
+    mypacer.setWindowSize(5);
+    mypacer.setItem("box_entry");
+    mypacer.setItem("frame_number_entry");
+    mypacer.setItem("frame_size");
+    char * item;
+    printf("My Dom ID: %d\n",mypacer.getDomid() );
+    printf("waiting for dom0...\n");
+    while (strcmp("init",item)!=0)
+    {
+        item = mypacer.readItem("frame_number_entry");
+    } 
+
+    mypacer.writeItem("frame_number_entry","ready");
+    printf("ready...\n");
+
+    while (strcmp("done",item)!=0 && strcmp("ready",item)!=0)
+    {
+        item=mypacer.readItem("frame_number_entry");
 
 
-        for (i = 0; i < 50; ++i)
-        {
+
+        // for (i = 0; i < 50; ++i)
+        // {
             matmult(ptr1,ptr2,ptr3,N);
 
 
@@ -92,20 +93,10 @@ int main(int argc, char** argv)
 
 
             // printf("heartbeat: instant rate: %f\n",hb_get_instant_rate(heart) );
-        }
+        // }
 
         
-        for (i = 0; i < 50; ++i)
-        {
 
-            matmult(ptr1,ptr2,ptr3,N);
-            mypacer.beat();
-            // write instant heart rate to xenstore
-            mypacer.writeInstantHeartRate();
-
-
-            // printf("heartbeat: instant rate: %f\n",hb_get_instant_rate(heart) );
-        }
         
     }
 
