7b027104b5906628eb221d08fb13fedd4d2332ea
Victor Chen <yvictorck@gmail.com>
2018-08-09 15:50:31 -0400

    a

--- Application/domU/cpp/matmult.cpp
+++ Application/domU/cpp/matmult.cpp
@@ -30,64 +30,78 @@ int main(int argc, char** argv)
     Pacer mypacer;
     // set window size for heartbeat and create heartbeat instance
     mypacer.setWindowSize(5);
-
-
-    int **ptr1, **ptr2, **ptr3;
-    int  col1, row2, col2;
-    int N=100;
-    int j,i;
-    ptr1 = (int **) malloc (sizeof (int *) * N);
-    ptr2 = (int **) malloc (sizeof (int *) * N);
-    ptr3 = (int **) malloc (sizeof (int *) * N);
-    for (i = 0; i < N; i++)
-        ptr1[i] = (int *) malloc (sizeof (int) * N);
-    for (i = 0; i < N; i++)
-        ptr2[i] = (int *) malloc (sizeof (int) * N);
-    for (i = 0; i < N; i++)
-        ptr3[i] = (int *) malloc (sizeof (int) * N);
-
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
-            ptr1[i][j] = rand ()%10;
-        }
-    }
-
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
-            ptr2[i][j] = rand ()%10;
-        }
-    }
-
-
-
-    for (i = 0; i < 50; ++i)
+    mypacer.setItem("box_entry");
+    mypacer.setItem("frame_number_entry");
+    mypacer.setItem("frame_size");
+    char * item;
+    printf("My Dom ID: %d\n",mypacer.getDomid() );
+    printf("waiting for dom0...\n");
+    mypacer.writeItem("frame_number_entry","ready");
+    printf("ready...\n");
+    while (strcmp("init",item)!=0)
     {
-        matmult(ptr1,ptr2,ptr3,N);
+        item = mypacer.readItem("frame_number_entry");
+    } 
 
-
-
-        mypacer.beat();
-        // write instant heart rate to xenstore
-        mypacer.writeInstantHeartRate();
-
-
-        // printf("heartbeat: instant rate: %f\n",hb_get_instant_rate(heart) );
-    }
-
-    
-    for (i = 0; i < 50; ++i)
+    while (strcmp("done",item)!=0)
     {
+        item=mypacer.readItem("frame_number_entry");
+        int **ptr1, **ptr2, **ptr3;
+        int  col1, row2, col2;
+        int N=100;
+        int j,i;
+        ptr1 = (int **) malloc (sizeof (int *) * N);
+        ptr2 = (int **) malloc (sizeof (int *) * N);
+        ptr3 = (int **) malloc (sizeof (int *) * N);
+        for (i = 0; i < N; i++)
+            ptr1[i] = (int *) malloc (sizeof (int) * N);
+        for (i = 0; i < N; i++)
+            ptr2[i] = (int *) malloc (sizeof (int) * N);
+        for (i = 0; i < N; i++)
+            ptr3[i] = (int *) malloc (sizeof (int) * N);
 
-        matmult(ptr1,ptr2,ptr3,N);
-        mypacer.beat();
-        // write instant heart rate to xenstore
-        mypacer.writeInstantHeartRate();
+        for (i = 0; i < N; i++) {
+            for (j = 0; j < N; j++) {
+                ptr1[i][j] = rand ()%10;
+            }
+        }
+
+        for (i = 0; i < N; i++) {
+            for (j = 0; j < N; j++) {
+                ptr2[i][j] = rand ()%10;
+            }
+        }
 
 
-        // printf("heartbeat: instant rate: %f\n",hb_get_instant_rate(heart) );
+
+        for (i = 0; i < 50; ++i)
+        {
+            matmult(ptr1,ptr2,ptr3,N);
+
+
+
+            mypacer.beat();
+            // write instant heart rate to xenstore
+            mypacer.writeInstantHeartRate();
+
+
+            // printf("heartbeat: instant rate: %f\n",hb_get_instant_rate(heart) );
+        }
+
+        
+        for (i = 0; i < 50; ++i)
+        {
+
+            matmult(ptr1,ptr2,ptr3,N);
+            mypacer.beat();
+            // write instant heart rate to xenstore
+            mypacer.writeInstantHeartRate();
+
+
+            // printf("heartbeat: instant rate: %f\n",hb_get_instant_rate(heart) );
+        }
+        
     }
-    
-
 
     printf("done\n");
     return 0;
